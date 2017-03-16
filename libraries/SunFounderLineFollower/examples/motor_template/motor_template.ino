8c868cf427bcd2c3805d6cb36a3c929a7904f8a3
Dave Pucknell <dave@pucknell.co.uk>
2017-03-16 10:00:56 +0000

    Added Anoter example for Line Follower

---
+++ libraries/SunFounderLineFollower/examples/motor_template/motor_template.ino
@@ -0,0 +1,54 @@
+#include <SunFounderLineFollower.h>
+
+SunFounderLineFollower sflf;
+int sensitivity = 9000;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int byteprocessed = sflf.byteprocessed(sensitivity);
+
+  if ( (bitRead(byteprocessed, 0)) || (bitRead(byteprocessed, 1)) || (bitRead(byteprocessed, 2)) ) {
+  //Right Turn
+  motorRight();
+  }
+
+  else if ( (bitRead(byteprocessed, 3)) || (bitRead(byteprocessed, 4)) ) {
+  //Forward
+  motorForward();
+  }
+
+  else if ( (bitRead(byteprocessed, 5)) || (bitRead(byteprocessed, 6)) || (bitRead(byteprocessed, 7)) ){
+  //Left Turn
+  motorLeft();
+  }
+  else {
+    //Lost the line... STOP!!!!
+    motorStop();
+  }
+
+  
+  delay(50); 
+}
+
+int motorRight() {
+  Serial.println("Turning Right");
+  //Put here motor right code....
+}
+int motorStop() {
+  Serial.println("LOST LINE");
+  //Put here motor stop code
+}
+int motorLeft() {
+  Serial.println("Turning Left");
+  //Put here motor Left Code
+}
+
+int motorForward() {
+  Serial.println("Forward!!!");
+  //Put here motor Forward!
+}
+
+
